19f865aff59fcb5b85875f91da0767dc8384576b
Murilo Terenciani <murilo.terenciani@gmail.com>
2021-10-19 22:27:30 -0300

    Layout da pokedex inicial

---
+++ src/pages/PokedexPage/styles.js
@@ -0,0 +1,61 @@
+import styled from "styled-components";
+
+export const PokedexPageContainer = styled.div `
+    height: 100vh;
+    background-image: linear-gradient(110deg,rgb(104 105 99 / 38%) 54.5%,#cc3342 50%, #cc3342 68%, #db5461 68%);
+    padding-top: 1.5%;
+`
+
+export const BarContainer = styled.div `
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    background-image: linear-gradient(110deg,#e3f2fd 62.1%,#1c3144 50%);
+`
+
+export const NumContainer = styled.div `
+    display: flex;
+    width: 80px;
+    align-items: center;
+    justify-content: space-evenly;
+    background-color: #696969;
+    border-radius: 25px;
+
+    img {
+        width: 30%;
+    }
+`
+
+export const MainContainer = styled.div `
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 80%;
+`
+
+export const PokemonContainer = styled.div `
+    display: flex;
+    flex-direction: column;
+    margin-left: 13%;
+    
+`
+
+export const ListPokedexContainer = styled.div `
+    display: flex;
+    align-items: center;
+    width: 25%;
+    background-color: aliceblue;
+    justify-content: space-between;
+    border-radius: 32px;
+    padding: 0px 5px;
+    margin-right: 1%;
+
+
+    img:nth-of-type(1) {
+        margin-bottom: 10px;
+    }
+
+    img:nth-of-type(2) {
+        width: 40px;
+    }
+`
